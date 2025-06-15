7098eb91e6986249ab298607077e65846e9df46a
kenhato <k.m14385277@gmail.com>
2025-06-15 18:35:41 +0900

    RestController追加（APIからのレスポンスを受け取る専用）6

--- springboot-jsp-app/src/main/java/com/example/demo/APIController.java
+++ springboot-jsp-app/src/main/java/com/example/demo/APIController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONObject; 
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -30,19 +31,24 @@ public class APIController {
     }
 
     private String getJWTTokenFromAws() {
-        try {
-            URL url = new URL(jwtApiUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("x-api-key", jwtApiKey);
+    try {
+        URL url = new URL(jwtApiUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("x-api-key", jwtApiKey);
 
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-                String response = reader.lines().collect(Collectors.joining());
-                return response.replace("{\"token\":\"", "").replace("\"}", "");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            String response = reader.lines().collect(Collectors.joining());
+
+            // Lambdaからのレスポンスが { "statusCode": 200, ..., "body": "{\"token\":\"xxx\"}" } の場合
+            JSONObject outer = new JSONObject(response);
+            JSONObject inner = new JSONObject(outer.getString("body"));
+
+            return inner.getString("token");
         }
+    } catch (IOException e) {
+        e.printStackTrace();
+        return null;
     }
+}
 }
